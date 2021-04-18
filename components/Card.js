97838ceecfc8a77558ae0d6fc8d558803e105bc3
blackandwhite <barandiaranlautaro@gmail.com>
2021-04-18 17:39:35 -0300

    retocando detalles

--- components/Card.js
+++ components/Card.js
@@ -5,16 +5,14 @@ export default function Card({name,image, number, id}){
     return(
         <>   
             <div className='col pe-1 me-2 ms-2 ps-1'>
-                <Link href={`/detail?id=${id}`}>
-                    <a>
+                <Link href={`/detail?id=${id}`}>      
                     <div className='card m-1 mx-auto mt-4 mb-5 bg-light shadow ' style={{width: '20em', cursor: 'pointer'}}>
                         <img src={ image } className='card-img-top m-auto ' alt={name} style={{width: '250px'}}/>
                             <div className='card-body bg-white'>
                                 <h5 className='card-title text-center text-capitalize'>{name}</h5>         
                                 <p className='text-center'>#{number}</p>
                             </div>
-                    </div>
-                    </a>
+                    </div>              
                 </Link>
             </div>
         </>       
